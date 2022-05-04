0be04218f3fcdd45ef48696dd6bb9860ed324835
chandan kumar sahoo <77827600+Chandan-cks@users.noreply.github.com>
2022-05-04 02:51:30 -0500

    Add files via upload

---
+++ typescript/types.js
@@ -0,0 +1,11 @@
+var student;
+var i;
+student = [
+    { id: "101", name: "chandan" },
+    { id: "102", name: "kumar" },
+    { id: "103", name: "sahoo" }
+];
+console.log(student);
+for (i = 0; i < student.length; i++) {
+    console.log(student[i]);
+}
---
+++ typescript/types.ts
@@ -0,0 +1,13 @@
+var student:any[];
+var i:number;
+student = [
+    { id: "101", name: "chandan" },
+    { id: "102", name: "kumar" },
+    { id: "103", name: "sahoo" }
+];
+
+console.log(student);
+
+for(i=0;i<student.length;i++){
+    console.log(student[i]);
+}
